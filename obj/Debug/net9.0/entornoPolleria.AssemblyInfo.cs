41bf02c4bfb84fb2445a4dc71ab298a730962a19
Valentino Olmedo <valenolme@gmail.com>
2025-07-21 16:13:24 -0300

    ahora si esta terminado index y alta xd

--- obj/Debug/net9.0/entornoPolleria.AssemblyInfo.cs
+++ obj/Debug/net9.0/entornoPolleria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("entornoPolleria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61de184816055687cc1e6c6e435ed6d8038718fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c1264bc0b648116f6766205eec5e6f15d6d42b")]
 [assembly: System.Reflection.AssemblyProductAttribute("entornoPolleria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("entornoPolleria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
