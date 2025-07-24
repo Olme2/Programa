8fc6b22e874a0e376316c214f4be1ce11c545bfd
Valentino Olmedo <valenolme@gmail.com>
2025-07-24 11:11:05 -0300

    commit modificar Promocion modifica bien, valida precio < costo, falta cuando crea nueva filas actualizar costos en vivo, pero al mandar 2 veces moidifca, dejar por ahora y avanzar a ventas

--- obj/Debug/net9.0/entornoPolleria.AssemblyInfo.cs
+++ obj/Debug/net9.0/entornoPolleria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("entornoPolleria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9635f249d661e640b7733d82ccf7aab5e1b6c92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59969d5c7c6e44c9e6b332d8caf7cd0bb15a8fa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("entornoPolleria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("entornoPolleria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
