b7b7671fd170909c1fbd81921abff53746618ff5
Valentino Olmedo <valenolme@gmail.com>
2025-07-25 13:28:07 -0300

    commit index muestra total, falta modificar y eliminar venta

--- obj/Debug/net9.0/entornoPolleria.AssemblyInfo.cs
+++ obj/Debug/net9.0/entornoPolleria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("entornoPolleria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ede6ed7604c8bff3f505ef4ecad9bb26a84bf1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525f8fc5c03b38d9713e2cad9f0083c6bce9589a")]
 [assembly: System.Reflection.AssemblyProductAttribute("entornoPolleria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("entornoPolleria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
